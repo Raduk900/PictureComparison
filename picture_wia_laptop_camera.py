6ad2986ff3813efed370cedab46202101a24f0ef
radoslav.titko <radoslav.titko@ringcentral.com>
2023-04-19 12:50:51 +0300

    Fixed making pictures wia camera

--- picture_wia_laptop_camera.py
+++ picture_wia_laptop_camera.py
@@ -3,7 +3,7 @@ import os
 
 def num_of_images():
 
-    pictures_path = "C:/Users/radek/PycharmProjects/bakalaur/Python/PictureComparison/Pictures"
+    pictures_path = "C:/Users/radtitk/Desktop/bakalaur/PictureComparison/Pictures"
     extensions = [".jpg", ".jpeg", ".png"]
 
     images = 0
@@ -13,21 +13,39 @@ def num_of_images():
             images += 1
 
     return images
-
-def capture_image():
+    
+def capture_with_screenshot():
+    
     counter = num_of_images()
 
-    imagePath = "C:/Users/radek/PycharmProjects/bakalaur/Python/PictureComparison/Pictures/image" + str(counter + 1)
+    imagePath = "C:/Users/radtitk/Desktop/bakalaur/PictureComparison/Pictures/image" + str(counter + 1) + ".jpg"
 
     cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error opening camera")
+        return
+
     while True:
         ret, frame = cap.read()
-        cv2.imshow("Press 'p' to capture image", frame)
-        key = cv2.waitKey(1)
-        if key == ord('p'):
-            cv2.imwrite(imagePath, frame)
+
+        if not ret:
+            print("Error reading frame")
             break
+
+        cv2.imshow('frame', frame)
+
+        key = cv2.waitKey(1)
+
+        if key == ord('p'):
+            # Take a screenshot and save it to a file
+            cv2.imwrite(imagePath, frame)
+            print("Screenshot taken!")
+
+        # Check if the key pressed is 'q'
+        if key == ord('q'):
+            break
+
     cap.release()
     cv2.destroyAllWindows()
 
-capture_image()
+capture_with_screenshot()
