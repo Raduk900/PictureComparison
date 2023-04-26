b93eb943316288fb26d545d14aba6161c3d4b602
radoslav.titko <radoslav.titko@ringcentral.com>
2023-04-26 22:41:27 +0300

    created tcp request

--- send.py
+++ send.py
@@ -1,17 +1,16 @@
-import requests
-import json
+import socket
+client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('192.168.43.2',3333))
+payload = str(1)
 
-# Define the API endpoint URL and the boolean value to send
-url = 'http://localhost:8000/api/endpoint'
-data = {
-    'boolean_param': True
-}
-
-# Convert the data to a JSON string
-json_data = json.dumps(data)
-
-# Send the POST request with JSON data
-response = requests.post(url, json=json_data)
-
-# Print the response content
-print(response.content)
+try:
+    while True:
+        client_socket.send(payload.encode('utf-8'))
+        more=input('Want to send more data to the server')
+        if more.lower()=='y':
+            payload=input("Enter Payload")
+        else:
+            break
+except KeyboardInterrupt:
+    print("Exited by user")
+client_socket.close()
