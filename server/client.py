852e374ce2faf7ec723dd7818166ea82746c47d8
radoslav.titko <radoslav.titko@ringcentral.com>
2023-04-28 16:15:53 +0300

    added server things

--- server/client.py
+++ server/client.py
@@ -1,14 +1,14 @@
 import socket
 client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.43.2',3333))
+client_socket.connect(('127.0.0.1',3333))
 payload = str(1)
 
 try:
     while True:
         client_socket.send(payload.encode('utf-8'))
-        more=input('Want to send more data to the server')
+        more=input('Want to send more data to the server press y: ')
         if more.lower()=='y':
-            payload=input("Enter Payload")
+            payload=input("Enter Payload: ")
         else:
             break
 except KeyboardInterrupt:
